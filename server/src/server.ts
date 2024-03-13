448c75ea681988602860bf2e34c7e9ee30f725fa
yuto.moriizumi <kurvan1112@gmail.com>
2024-03-13 17:54:06 +0900

    Revert "一旦validateを無効化し、formatが動く状態にした"
    This reverts commit 064f59d2bb8d093d3e9941febfb6be9e8f987dbc.

--- server/src/server.ts
+++ server/src/server.ts
@@ -75,9 +75,9 @@ function main() {
       );
     }
     // Revalidate all open text documents
-    // documents いったんコメントアウト
-    //   .all()
-    //   .forEach((document) => validateTextDocument(context, settings, document));
+    documents
+      .all()
+      .forEach((document) => validateTextDocument(context, settings, document));
   });
 
   // Only keep settings for open documents
@@ -88,7 +88,7 @@ function main() {
   // The content of a text document has changed. This event is emitted
   // when the text document first opened or when its content has changed.
   documents.onDidChangeContent((change) => {
-    // validateTextDocument(context, settings, change.document);
+    validateTextDocument(context, settings, change.document);
   });
 
   connection.onDidChangeWatchedFiles((_change) => {
