dec1b9caa022eb2dff3d7c16f9b335d7746c04b6
volgakurvar <kurvan1112@gmail.com>
2024-08-29 01:35:42 +0900

    int_variable_fieldを追加

--- server/src/validator/rule/variable_field.ts
+++ server/src/validator/rule/variable_field.ts
@@ -1,6 +1,12 @@
 import { EntryDescriptor, Value } from "./types";
 
+// TODO: Rename this to float_variable_field or something better
 export const variable_field: EntryDescriptor[] = [
   { type: Value.FLOAT },
+  { type: Value.REFERENCE_TO, tag: "variable" },
+];
+
+export const int_variable_field: EntryDescriptor[] = [
+  { type: Value.INT },
   { type: Value.REFERENCE_TO, tag: "variable" },
 ];
