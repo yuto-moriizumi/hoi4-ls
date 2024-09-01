34ba312a915008891f69deb3cf1927546074ff53
volgakurvar <kurvan1112@gmail.com>
2024-09-01 18:42:35 +0900

    プロンプトを別ファイルに分離

--- converter/src/convert/index.ts
+++ converter/src/convert/index.ts
@@ -2,7 +2,7 @@ import { mkdir, readFile, writeFile } from "fs/promises";
 import { ChatOpenAI } from "@langchain/openai";
 import { PromptTemplate, FewShotPromptTemplate } from "@langchain/core/prompts";
 import { EXAMPLES } from "./examples";
-import path from "path";
+import path, { join } from "path";
 
 import dotenv from "dotenv";
 dotenv.config();
@@ -12,7 +12,7 @@ const model = new ChatOpenAI({
   openAIApiKey: process.env.OPENAI_API_KEY,
 });
 
-async function loadExample(filePath: string) {
+async function loadText(filePath: string) {
   return (await readFile(filePath))
     .toString()
     .replaceAll("{", "{{")
@@ -27,88 +27,12 @@ async function getTemplate() {
     ),
     examples: await Promise.all(
       EXAMPLES.map(async ([inputPath, outputPath]) => ({
-        before: await loadExample(inputPath),
-        after: await loadExample(outputPath),
+        before: await loadText(inputPath),
+        after: await loadText(outputPath),
       })),
     ),
-    prefix: `Your task is to convert the given code to typescript.
-Given code represents the set of syntaxes and its rules to satisfy.
-# Basic structure
-Overall structure will look like this
-"pdxparticle = {
-		name = scalar
-		type = <particle>
-
-		## cardinality = 0..1
-		scale = float
-}"
-This defines the syntax rule of the pdxparticle.
-It has following entries;
-- "name" key, and which value should satisfy "scalar" rule.
-- "type" key, and which value should satisfy "<particle>" rule.
-- "scale" key, and which value should satisfy "float" rule.
-You have to convert this to typescript as follows;
-"const pdxparticle = obj({
-    name: scalar()
-    type: typeRef("particle")
-    scale = float({cardinality:[0,1]})
-  })"
-
-# Attributes
-"##" is the attribute symbol. It describes the restriction of the following entry.
-For example,
-"## cardinality = 0..1
-scale = float"
-This means "scale" entry has the "cardinality" attribute with the value of 0..1.
-
-# Simple rules
-Theres simple rules like primitive values.
-- bool
-
-# Rule 1 - 
-# Rule 1 - Items without \`=\` and values in the brackets represents the Array
-Please note that some brackets might represent the Array. For example...
-color = {{
-  ## cardinality = 3..3
-  int
-}}
-color = { type: Value.ARRAY, values: [int]}
-This is the array, because the items inside (int) doesn't have \`=\`
-Also, if you see "subtype", that means the set of values are mutually execlusive relationship. For example,
-country_tag_alias = {{
-  subtype[variable] = {{
-    variable = variable_field
-  }}
-  subtype[global_event_target] = {{
-    global_event_target = value[global_event_target]
-  }}
-  fallback = enum[explicit_country_tags]
-}}
-This case, country_tag_alias is allowed to have "fallback = enum[explicit_country_tags]"
-but "variable = variable_field" and "global_event_target = value[global_event_target]" can be coexited.
-for this case it can be converted like this. Converted code will use typescript array to express the mutually exclusive relationship.
-country_tag_alias = {{
-  children: [
-    {{
-      // variable
-      children: {{
-        fallback: enum("explicit_country_tags"),
-        variable: variable_field,
-      }},
-    }},
-    {{
-      // global_event_target
-      children: {{
-        fallback: enum("explicit_country_tags"),
-        global_event_target: value("global_event_target"),
-      }},
-    }}
-  ]
-}}
-There's a constant named "variable_field" and "int_variable_field" available so when you find it feel free to use.
-Refer to the following examples;
-`,
-    suffix: "Convert this code to typescript:\n```{input}```",
+    prefix: await loadText(join(__dirname, "prefix.txt")),
+    suffix: "Convert this code to TS format:\n```{input}```",
     partialVariables: { input: "string" },
   });
 }
