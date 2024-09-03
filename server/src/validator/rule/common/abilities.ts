e12da8e58a68b25547cc0c19c3db3ed286bc7623
yuto.moriizumi <kurvan1112@gmail.com>
2024-09-03 18:33:20 +0900

    enum変換精度上げた

---
+++ server/src/validator/rule/common/abilities.ts
@@ -0,0 +1,67 @@
+import { effect } from "../effects";
+import { modifier_rule } from "../modifier_rule";
+import { modifier } from "../modifiers";
+import { trigger } from "../triggers";
+import { unit_stat } from "../unit_stats";
+import {
+  obj,
+  unit_leader,
+  country,
+  localisation,
+  typeRef,
+  scalar,
+  enumRef,
+  float,
+  int,
+  bool,
+  enumRefKey,
+} from "../utils";
+
+export const ability_unit_leader_types = ["army_leader"];
+
+export const ability = obj(
+  {
+    replace_scope: {
+      this: unit_leader(),
+      root: unit_leader(),
+      from: country(),
+    },
+  },
+  {
+    name: localisation(),
+    desc: localisation(),
+    icon: typeRef({ cardinality: [0, 1], severity: "warning" }, "spriteType"),
+    sound_effect: scalar({ cardinality: [0, 1] }),
+    type: enumRef({}, "ability_unit_leader_types"),
+    allowed: obj(
+      { cardinality: [0, 1] },
+      {
+        ...trigger,
+      },
+    ),
+    cost: float(),
+    duration: int(),
+    cooldown: int({ cardinality: [0, 1] }),
+    unit_modifiers: obj(
+      { cardinality: [0, 1] },
+      {
+        ...modifier,
+        ...unit_stat,
+      },
+    ),
+    one_time_effect: obj(
+      { cardinality: [0, 1] },
+      {
+        ...effect,
+      },
+    ),
+    cancelable: bool({ cardinality: [0, 1] }),
+    ai_will_do: obj(
+      { cardinality: [0, 1] },
+      {
+        [enumRefKey("base_factor")]: float({ cardinality: [0, 1] }),
+        ...modifier_rule,
+      },
+    ),
+  },
+);
