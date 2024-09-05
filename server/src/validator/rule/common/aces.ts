58d51fdcde08075a4d90c37458e3bce880483313
yuto.moriizumi <kurvan1112@gmail.com>
2024-09-05 13:51:32 +0900

    aces

--- server/src/validator/rule/common/aces.ts
+++ server/src/validator/rule/common/aces.ts
@@ -1,18 +1,40 @@
-import { modifiers } from "../modifiers";
-import { Value, Scope, RootObjectEntryDescriptor } from "../types";
-import { Enum, float } from "../utils";
+import { modifier } from "../modifiers";
+import {
+  obj,
+  enumRef,
+  enumRefKey,
+  float,
+  root,
+  typeDefKey,
+  either,
+  array,
+} from "../utils";
 
-export const ace: RootObjectEntryDescriptor = {
-  rootKey: "modifiers",
-  children: {
-    type: [Enum("air_units"), { type: Value.ARRAY, values: Enum("air_units") }],
+const ace = obj(
+  {},
+  {
+    type: either(
+      enumRef({}, "air_units"),
+      array({}, [enumRef({ cardinality: [~1, Infinity] }, "air_units")]),
+    ),
     chance: float(),
-    effect: {
-      replaceScope: {
-        root: Scope.AIR,
-        this: Scope.AIR,
+    effect: obj(
+      { replace_scope: { ROOT: air(), THIS: air() } },
+      {
+        ...modifier,
       },
-      children: modifiers,
-    },
+    ),
   },
-};
+);
+
+export const aceType = root(
+  { path: "game/common/aces" },
+  {
+    modifiers: obj(
+      {},
+      {
+        [typeDefKey("ace")]: ace,
+      },
+    ),
+  },
+);
