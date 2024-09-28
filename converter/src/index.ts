f52aa12ec64b2b7bdb6ef10b43f4d65540ed3f5c
volgakurvar <kurvan1112@gmail.com>
2024-09-28 22:01:17 +0900

    a

--- converter/src/index.ts
+++ converter/src/index.ts
@@ -1,17 +1,17 @@
 import { program } from "@commander-js/extra-typings";
 import { convertAll } from "./convertAll";
 import { convert } from "./convert";
+import { parse } from "./parse";
 
-program.command("parse").action(() => {
-  console.log("im parse");
-});
+program
+  .command("parse")
+  .argument("<filePath>", "Relative path to the file")
+  .action(parse);
 
 program
   .command("convert")
   .argument("<filePath>", "Relative path to the file")
-  .action((filePath) => {
-    convert(filePath);
-  });
+  .action(convert);
 
 program.command("convert-all").action(convertAll);
 
