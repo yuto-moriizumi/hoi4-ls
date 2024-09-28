684ccd0f213fe9b4043f631080c0e2a203a0bde4
volgakurvar <kurvan1112@gmail.com>
2024-09-28 22:41:59 +0900

    autonomous_states

--- server/src/validator/rule/common/autonomous_states.ts
+++ server/src/validator/rule/common/autonomous_states.ts
@@ -1,85 +1,82 @@
-import { triggers } from "../triggers";
 import { modifier_rule } from "../modifier_rule";
-import { modifiers } from "../modifiers";
+import { modifier } from "../modifiers";
+import { trigger } from "../triggers";
 import {
-  EntryDescriptor,
-  RootObjectEntryDescriptor,
-  Scope,
-  Value,
-} from "../types";
-import { Enum, localisation, number } from "../utils";
+  root,
+  obj,
+  typeDefKey,
+  country,
+  localisation,
+  bool,
+  float,
+  enumRefKey,
+  array,
+  typeRef,
+} from "../utils";
 
-export const autonomy: RootObjectEntryDescriptor = {
-  replaceScope: {
-    this: Scope.COUNTRY,
-    root: Scope.COUNTRY,
+export const autonomyType = root(
+  { path: "game/common/autonomous_states" },
+  {
+    autonomy: obj(
+      {},
+      {
+        [typeDefKey("autonomy")]: obj(
+          { replace_scope: { this: country(), root: country() } },
+          {
+            id: localisation(),
+            default: bool({ cardinality: [0, 1] }),
+            is_puppet: bool({ cardinality: [0, 1] }),
+            use_overlord_color: bool({ cardinality: [0, 1] }),
+            min_freedom_level: float({}, 0, 0.99),
+            peace_conference_initial_freedom: float(
+              { cardinality: [0, 1] },
+              0,
+              1,
+            ),
+            manpower_influence: float({}, 0, 1),
+            rule: obj(
+              {},
+              {
+                desc: localisation({ cardinality: [0, 1] }),
+                [enumRefKey("game_rules")]: bool({ cardinality: [0, 20] }),
+              },
+            ),
+            modifier: obj({ cardinality: [1, Infinity] }, { ...modifier }),
+            ai_subject_wants_higher: obj(
+              {},
+              {
+                factor: float(),
+              },
+            ),
+            ai_overlord_wants_lower: obj(
+              {},
+              {
+                factor: float(),
+              },
+            ),
+            ai_overlord_wants_garrison: obj(
+              { cardinality: [0, 1] },
+              { ...trigger },
+            ),
+            allowed: obj({ cardinality: [0, 1] }, { ...trigger }),
+            allowed_levels_filter: array({ cardinality: [0, 1] }, [
+              typeRef({ cardinality: [1, Infinity] }, "autonomy"),
+            ]),
+            use_for_peace_conference_weight: obj(
+              {
+                replace_scope: { root: country(), from: country() },
+                cardinality: [0, 1],
+              },
+              {
+                [enumRefKey("base_factor")]: float({ cardinality: [0, 1] }),
+                ...modifier_rule,
+              },
+            ),
+            can_take_level: obj({ cardinality: [0, 1] }, { ...trigger }),
+            can_lose_level: obj({ cardinality: [0, 1] }, { ...trigger }),
+          },
+        ),
+      },
+    ),
   },
-  children: {
-    id: localisation(),
-    default: { type: Value.BOOL, cardinality: [0, 1] },
-    is_puppet: { type: Value.BOOL, cardinality: [0, 1] },
-    use_overlord_color: { type: Value.BOOL, cardinality: [0, 1] },
-    min_freedom_level: { type: Value.FLOAT, range: [0, 0.99] },
-    peace_conference_initial_freedom: {
-      type: Value.FLOAT,
-      cardinality: [0, 1],
-      range: [0, 1],
-    },
-    manpower_influence: { type: Value.FLOAT, range: [0, 1] },
-    rule: {
-      children: {
-        desc: { type: Value.LOCALISATION, cardinality: [0, 1] },
-        game_rules: { type: Value.BOOL, cardinality: [0, 20] },
-      },
-    },
-    modifier: {
-      cardinality: [1, Infinity],
-      children: modifiers,
-    },
-    ai_subject_wants_higher: {
-      children: {
-        factor: number(),
-      },
-    },
-    ai_overlord_wants_lower: {
-      children: {
-        factor: number(),
-      },
-    },
-    ai_overlord_wants_garrison: {
-      cardinality: [0, 1],
-      children: triggers,
-    },
-    allowed: {
-      cardinality: [0, 1],
-      children: triggers,
-    },
-    allowed_levels_filter: {
-      type: Value.ARRAY,
-      values: { type: Value.REFERENCE_TO, tag: "autonomy" },
-      cardinality: [1, Infinity],
-    },
-    use_for_peace_conference_weight: {
-      replaceScope: {
-        root: Scope.COUNTRY,
-        from: Scope.COUNTRY,
-      },
-      cardinality: [0, 1],
-      children: modifier_rule,
-      dynamicChildren: [
-        {
-          key: Enum("base_factor"),
-          value: number(),
-        },
-      ],
-    } satisfies EntryDescriptor,
-    can_take_level: {
-      cardinality: [0, 1],
-      children: triggers,
-    },
-    can_lose_level: {
-      cardinality: [0, 1],
-      children: triggers,
-    },
-  },
-};
+);
