f91c18123bcf4502fa3f8a3194c3d707fce23922
yuto.moriizumi <kurvan1112@gmail.com>
2024-10-11 18:08:31 +0900

    static_modifiers

---
+++ server/src/validator/rule/common/static_modifiers.ts
@@ -0,0 +1,47 @@
+import { modifier } from "../modifiers";
+import { unit_stat, air_stat, naval_stat } from "../temp_modifiers";
+import { trigger } from "../triggers";
+import { either, obj, root, typeDefKey, array } from "../utils";
+
+const static_modifier = either(
+  obj(
+    {},
+    {
+      ...modifier,
+      ...unit_stat,
+      ...air_stat,
+      ...naval_stat,
+    },
+  ),
+  obj(
+    {},
+    {
+      valid_relation_trigger: obj(
+        {},
+        {
+          ...trigger,
+        },
+      ),
+    },
+  ),
+);
+
+export const staticModifierType = root(
+  { path: "game/common/modifiers" },
+  {
+    modifier: either(
+      obj(
+        {},
+        {
+          [typeDefKey("static_modifier")]: static_modifier,
+        },
+      ),
+      obj(
+        {},
+        {
+          valid_relation_trigger: array({}, []),
+        },
+      ),
+    ),
+  },
+);
