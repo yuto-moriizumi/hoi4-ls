7fcfee1b6c63f0dad5c7df5e1f4fd0266b2e81eb
yuto.moriizumi <kurvan1112@gmail.com>
2024-10-11 18:43:53 +0900

    unit_medals

---
+++ server/src/validator/rule/common/unit_medals.ts
@@ -0,0 +1,52 @@
+import { effect } from "../effects";
+import { modifier } from "../modifiers";
+import { trigger } from "../triggers";
+import {
+  obj,
+  country,
+  int,
+  typeRef,
+  either,
+  variable_field,
+  root,
+  typeDefKey,
+} from "../utils";
+
+const unit_medal = obj(
+  { replace_scope: { root: country(), this: country() } },
+  {
+    available: obj(
+      { cardinality: [0, 1] },
+      {
+        ...trigger,
+      },
+    ),
+    frame: int(),
+    icon: typeRef({}, "spriteType"),
+    cost: either(...variable_field(), int()),
+    unit_modifiers: obj(
+      { cardinality: [0, 1] },
+      {
+        ...modifier,
+      },
+    ),
+    one_time_effect: obj(
+      { cardinality: [0, 1] },
+      {
+        ...effect,
+      },
+    ),
+  },
+);
+
+export const unitMedalType = root(
+  { path: "game/common/unit_medals" },
+  {
+    unit_medals: obj(
+      {},
+      {
+        [typeDefKey("unit_medal")]: unit_medal,
+      },
+    ),
+  },
+);
