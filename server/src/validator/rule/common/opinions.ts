0ba1bb8a97d2d915daebcae029823d3c51f6dd78
yuto.moriizumi <kurvan1112@gmail.com>
2024-10-11 17:39:34 +0900

    opinions

---
+++ server/src/validator/rule/common/opinions.ts
@@ -0,0 +1,28 @@
+import { obj, int, bool, float, enumRefKey, root, typeDefKey } from "../utils";
+
+const opinion = obj(
+  {},
+  {
+    value: int(),
+    trade: bool({ cardinality: [0, 1] }),
+    decay: float({ cardinality: [0, 1] }),
+    min_trust: int({ cardinality: [0, 1] }),
+    max_trust: int({ cardinality: [0, 1] }),
+    target: bool({ cardinality: [0, 1] }),
+    [enumRefKey("opinion_timer")]: int({ cardinality: [0, 1] }),
+  },
+);
+
+export const opinion_timer = ["days", "months", "years"];
+
+export const opinionType = root(
+  { path: "game/common/opinion_modifiers" },
+  {
+    opinion_modifiers: obj(
+      {},
+      {
+        [typeDefKey("opinion")]: opinion,
+      },
+    ),
+  },
+);
