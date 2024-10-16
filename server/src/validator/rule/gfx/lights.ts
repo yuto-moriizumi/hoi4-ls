bf66507b6473f697ab7060f145739196fd754bc1
yuto.moriizumi <kurvan1112@gmail.com>
2024-10-16 18:34:04 +0900

    lights

---
+++ server/src/validator/rule/gfx/lights.ts
@@ -0,0 +1,91 @@
+import {
+  root,
+  obj,
+  typeDefKey,
+  literal,
+  scalar,
+  array,
+  float,
+  int,
+  bool,
+  enumRef,
+  either,
+} from "../utils";
+
+const light = obj(
+  {},
+  {
+    name: scalar(),
+    color: obj(
+      {},
+      {
+        r: array({}, [float()]),
+        g: array({}, [float()]),
+        b: array({}, [float()]),
+      },
+    ),
+    intensity: scalar({ cardinality: [0, 1] }),
+    radius: float(),
+    falloff: float(),
+    duration: float({ cardinality: [0, 1] }),
+    position: obj(
+      {},
+      {
+        x: int(),
+        y: int(),
+        z: int(),
+      },
+    ),
+    animation: obj(
+      { cardinality: [0, 1] },
+      {
+        name: scalar(),
+        start: float(),
+        duration: float(),
+        repeat: bool({ cardinality: [0, 1] }),
+        op: enumRef({}, "animation_op"),
+        minValue: float({ cardinality: [0, 1] }),
+        maxValue: float({ cardinality: [0, 1] }),
+        curve: array({ cardinality: [4, Infinity] }, [float()]),
+      },
+    ),
+  },
+);
+
+export const intensity = either(
+  scalar({ cardinality: [0, Infinity] }),
+  int({ cardinality: [0, Infinity] }),
+  enumRef({ cardinality: [0, Infinity] }, "light_animations"),
+);
+
+export const animation_op = ["MUL", "ADD", "ABS"];
+
+export const light_animations = obj(
+  {},
+  {
+    path: literal("game/gfx/entities"),
+    name: obj(
+      {},
+      {
+        animation: obj(
+          {},
+          {
+            name: literal("enum_name"),
+          },
+        ),
+      },
+    ),
+  },
+);
+
+export const lightType = root(
+  { path: "game/gfx/entities" },
+  {
+    light: obj(
+      {},
+      {
+        [typeDefKey("light")]: light,
+      },
+    ),
+  },
+);
