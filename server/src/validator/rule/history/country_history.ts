b40ac62600b752e3fa778a400ff3307afbc80d04
volgakurvar <kurvan1112@gmail.com>
2024-10-16 21:07:27 +0900

    country_history

---
+++ server/src/validator/rule/history/country_history.ts
@@ -0,0 +1,53 @@
+import { effect } from "../effects";
+import {
+  obj,
+  typeRef,
+  either,
+  value,
+  scopeRef,
+  float,
+  root,
+  typeDefKey,
+} from "../utils";
+
+const country_history = obj(
+  {},
+  {
+    capital: typeRef({}, "state"),
+    oob: typeRef({ cardinality: [0, 1] }, "oob"),
+    ...effect,
+    recruit_character: either(
+      typeRef({ cardinality: [0, Infinity] }, "character"),
+      value({ cardinality: [0, Infinity] }, "event_target"),
+      value({ cardinality: [0, Infinity] }, "global_event_target"),
+      scopeRef({ cardinality: [0, Infinity] }, "character"),
+    ),
+    starting_train_buffer: float({ cardinality: [0, Infinity] }),
+    date_field: obj(
+      { cardinality: [0, Infinity] },
+      {
+        capital: typeRef({ cardinality: [0, 1] }, "state"),
+        oob: typeRef({ cardinality: [0, Infinity] }, "oob"),
+        ...effect,
+        recruit_character: either(
+          typeRef({ cardinality: [0, Infinity] }, "character"),
+          value({ cardinality: [0, Infinity] }, "event_target"),
+          value({ cardinality: [0, Infinity] }, "global_event_target"),
+          scopeRef({ cardinality: [0, Infinity] }, "character"),
+        ),
+      },
+    ),
+  },
+);
+
+export const countryHistoryType = root(
+  { path: "game/history/countries" },
+  {
+    country_history: obj(
+      {},
+      {
+        [typeDefKey("country_history")]: country_history,
+      },
+    ),
+  },
+);
